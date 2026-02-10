e51e2008f27fb5a24a1dc46cd49cf04210e589a4
TheTonicStudios <TheTonicStudios@users.noreply.github.com>
2026-02-09 23:50:45 -0500

    Remove webpack-dev-server resolution, simplify CRACO to use dev-server 4.x

--- client/craco.config.js
+++ client/craco.config.js
@@ -1,28 +1,5 @@
 /**
- * CRACO config: use webpack-dev-server 5 API (setupMiddlewares, server) so we can
- * run patched webpack-dev-server@5.2.1 and fix moderate security advisories.
- * React-scripts passes deprecated top-level `https`; convert it to the `server` option.
+ * CRACO config: use react-scripts' default webpack-dev-server (4.x), which accepts
+ * the top-level `https` option. No overrides needed; resolution warning is avoided.
  */
-module.exports = {
-  devServer: (devServerConfig) => {
-    const onBefore = devServerConfig.onBeforeSetupMiddleware;
-    const onAfter = devServerConfig.onAfterSetupMiddleware;
-    delete devServerConfig.onBeforeSetupMiddleware;
-    delete devServerConfig.onAfterSetupMiddleware;
-    devServerConfig.setupMiddlewares = (middlewares, devServer) => {
-      if (typeof onBefore === 'function') onBefore(devServer);
-      if (typeof onAfter === 'function') onAfter(devServer);
-      return middlewares;
-    };
-    // webpack-dev-server 5: top-level "https" is invalid; use "server" instead
-    const https = devServerConfig.https;
-    delete devServerConfig.https;
-    if (https) {
-      devServerConfig.server =
-        typeof https === 'object' && (https.key || https.cert)
-          ? { type: 'https', options: https }
-          : 'https';
-    }
-    return devServerConfig;
-  },
-};
+module.exports = {};
